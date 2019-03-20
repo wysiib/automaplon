b367a4c7af85ffca6f293ad83d3b66dce9c5772a
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-03-20 17:19:51 +0100

    format code according to coding guidelines

--- test/test_util.pl
+++ test/test_util.pl
@@ -1,12 +1,11 @@
-:- module(test_util,[cleanup_state/0, 
+:- module(test_util,[cleanup_state/0,
                      equal_lists_as_set/2]).
 
-
-equal_lists_as_set(A,B) :- 
-    list_to_ord_set(A,SA) , 
-    list_to_ord_set(B,SB) , 
+equal_lists_as_set(A,B) :-
+    list_to_ord_set(A,SA),
+    list_to_ord_set(B,SB),
     SA == SB.
 
 cleanup_state :-
-    retract(state:next_id(_)) ,
+    retract(state:next_id(_)),
     assert(state:next_id(0)).
