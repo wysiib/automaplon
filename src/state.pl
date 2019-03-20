f8a89756bf9665b83beb592b2394eb9ac9ba29a6
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-03-20 17:19:00 +0100

    add attribute number to states, add set_number/2, add get_number/2

--- src/state.pl
+++ src/state.pl
@@ -1,13 +1,16 @@
 :- module(state, [new_state/1,
                   is_accept/1,
                   reset_transitions/1,
-                  get_id/2,
-                  get_next_states/2,
-                  get_transitions/2,
-                  get_transitions/3,
                   add_transition/2,
                   set_accept/2,
                   equals/2,
+                  set_number/2,
+                  get_number/2,
+                  get_id/2,
+                  get_accept/2,
+                  get_next_states/2,
+                  get_transitions/2,
+                  get_transitions/3,
                   step/3]).
 
 :- use_module(util/maps).
@@ -23,6 +26,7 @@
 %   accept - is true if state is accepting
 %   transitions - a map associating a literal to a list of states, the list might contain duplicates
 %   next_states - an ordered set of states reachable in one step
+%   number - a number which is not necessarily unique among states (we are then able to enumerate a set of states and, for instance, use a bitset)
 
 :- dynamic next_id/1.
 :- volatile next_id/1.
@@ -34,53 +38,60 @@ get_next_id(N) :-
     NN is N + 1,
     assert(next_id(NN)).
 
-%% get_id(+State, -Id).
-%
-% Return a state's id.
-get_id(State, Id) :- 
-    get_attr(State, id, Id).
-
 %% new_state(-State).
 %
 % Instantiates a fresh automaton state.
 new_state(State) :-
     get_next_id(NextId),
-    put_attr(State, id, NextId) , 
+    put_attr(State, id, NextId),
+    put_attr(State, accept, false),
+    put_attr(State, transitions, transitions{}),
     put_attr(State, next_states, []).
 
+%% get_id(+State, -Id).
+%
+% True if Id is the state's id.
+get_id(State, Id) :-
+    get_attr(State, id, Id).
+
+%% get_number(+State, -Number).
+%
+% True if Number is the State's ground number, otherwise false.
+get_number(State, Number) :-
+    get_attr(State, number, Number).
+
+%% set_number(+State, -Id).
+%
+% Set a state's number.
+set_number(State, Number) :-
+    put_attr(State, number, Number).
+
 %% get_next_states(+State, -NextStates).
-% 
-% Return the set of states reachable in one step.
-get_next_states(State, NextStates) :- 
+%
+% True if NextStates is the set of states reachable in one step.
+get_next_states(State, NextStates) :-
     get_attr(State, next_states, NextStates).
 
 %% get_transitions(+State, -Transitions).
 %
-% Access the data structure in which the all transitions are stored
-% which lead away from State.
-% The output variable Transitions might be unified with an empty data structure
-% If no transitions are attached to the State.
+% True if Transitions is the data structure of transitions from State.
 get_transitions(State, Transitions) :-
-    get_attr(State, transitions, Transitions) ,
-    !.
-get_transitions(_, transitions{}).
+    get_attr(State, transitions, Transitions).
 
-%% get_transitions(+State, +Literal, -DestinationStates).
+%% get_transitions(+State, +Lit, -DestinationStates).
 %
-% Return a list of destination states reachable from State by transitioning
-% with Literal.
+% True if Destinations is a list of destination states reachable from State by transitioning
+% with Lit.
 get_transitions(State, Lit, Destinations) :-
     step(State, Lit, Destinations).
 
 %% step(+State, +Literal, -DestinationStates) is det.
 %
-% Return a list of destination states reachable from State by transitioning
-% with Literal.
-% If no state is reachable by the specified literal the resulting list will
-% be empty.
+% True if Destinations is a list of destination states reachable from State by transitioning
+% with Lit. If no state is reachable by the specified literal Destinations will be the empty list.
 step(State, Lit, Destinations) :-
-    get_transitions(State, Transitions) ,
-    map_get(Transitions, Lit, Destinations) ,
+    get_transitions(State, Transitions),
+    map_get(Transitions, Lit, Destinations),
     !.
 step(_, _, []).
 
@@ -94,9 +105,9 @@ step(_, _, []).
 % For instance, =|add_transition(S1, [a,d]-S2)|= allows to transition from
 % state S1 to S2 with either literal from a to d.
 add_transition(State, [Min,Max]-Destination) :-
-    add_transition_range(State, Min, Max, Destination) , 
-    get_attr(State, next_states, OldNextStates) , 
-    ord_add_element(OldNextStates, Destination, NewNextStates) , 
+    add_transition_range(State, Min, Max, Destination),
+    get_attr(State, next_states, OldNextStates),
+    ord_add_element(OldNextStates, Destination, NewNextStates),
     put_attr(State, next_states, NewNextStates).
 
 %% add_transition(+State, +Literal, +Destination).
@@ -104,10 +115,10 @@ add_transition(State, [Min,Max]-Destination) :-
 % Add a single transition to the Destination state from State by using the Literal.
 % Note: only to be used in add_transition/2, attribute next_states is not updated here!
 add_transition(State, Lit, Destination) :-
-    get_transitions(State, Transitions) ,
-    (map_get(Transitions, Lit, Destinations) ; Destinations = []) , % TODO: use a set?
-    ! , 
-    map_assoc(Transitions, Lit, [Destination|Destinations], NewTransitions) , 
+    get_transitions(State, Transitions),
+    (map_get(Transitions, Lit, Destinations) ; Destinations = []), % TODO: use a set?
+    !,
+    map_assoc(Transitions, Lit, [Destination|Destinations], NewTransitions),
     put_attr(State, transitions, NewTransitions).
 
 %% add_transition_range(+State, +Min, +Max, +Dest).
@@ -139,22 +150,22 @@ add_transition_range([L|Ls], State, Dest) :-
 % L = [a, b, c, d].
 % ===
 literals_list(Min, Max, Literals) :-
-    atom_codes(Min, [AsciiMin]) ,
-    atom_codes(Max, [AsciiMax]) ,
-    numlist(AsciiMin, AsciiMax, Asciis) ,
+    atom_codes(Min, [AsciiMin]),
+    atom_codes(Max, [AsciiMax]),
+    numlist(AsciiMin, AsciiMax, Asciis),
     asciis_to_atom_codes(Asciis,D-D,Literals).
 
 asciis_to_atom_codes([], Acc-D, Acc) :- D = [].
-asciis_to_atom_codes([Ascii|T], Acc-D, Literals) :- 
-    atom_codes(Codes,[Ascii]) , 
-    D = [Codes|ND] , 
+asciis_to_atom_codes([Ascii|T], Acc-D, Literals) :-
+    atom_codes(Codes,[Ascii]),
+    D = [Codes|ND],
     asciis_to_atom_codes(T, Acc-ND, Literals).
 
 %% reset_transitions(+State).
 %
 % Remove all transitions attached to State.
 reset_transitions(State) :-
-    put_attr(State, transitions, transitions{}) , 
+    put_attr(State, transitions, transitions{}),
     put_attr(State, next_states, []).
 
 %% set_accept(+State, +Accept).
@@ -164,11 +175,17 @@ reset_transitions(State) :-
 set_accept(State, Accept) :-
     put_attr(State, accept, Accept).
 
+%% get_accept(+State, -Accept).
+%
+% True if Accept is the acceptance attribute of State, i.e., either the atom true or false.
+get_accept(State, Accept) :-
+    get_attr(State, accept, Accept).
+
 %% is_accept(+State).
 %
-% Succeeds if State is accepting.
+% True if State is accepting.
 is_accept(State) :-
-    get_attr(State, accept, Accept) ,
+    get_attr(State, accept, Accept),
     Accept == true.
 
 %% is_accept(+State1, +State2).
@@ -178,8 +195,8 @@ equals(State1, State2) :-
     State1 == State2.
 
 %% Hooks for unification of a State variable's attributes.
-% We do not allow unification of two States but check for equality only, i.e., 
+% We do not allow unification of two States but check for equality only, i.e.,
 % both states have the same attribute id (both States are exactly the same).
-id:attr_unify_hook(IdX, Y) :- 
-    get_id(Y, IdY) , 
+id:attr_unify_hook(IdX, Y) :-
+    get_id(Y, IdY),
     IdX == IdY.
